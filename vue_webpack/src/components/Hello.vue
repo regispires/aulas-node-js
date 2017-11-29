dc7db7f8ef5a9c376a39d1cefc997e863aebb816
Regis Pires Magalhães <regispiresmag@gmail.com>
2017-11-28 21:01:24 -0300

    added projects vue_simple and vue_webpack

---
+++ vue_webpack/src/components/Hello.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="hello">
+    <h1 v-bind:title="titulo" >{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      msg: 'Bem vindo a sua primeira app com Vue.js',
+      titulo: 'UFC Quixadá'
+    }
+  }
+}
+</script>
